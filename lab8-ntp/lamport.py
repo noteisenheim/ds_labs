3514d854580482dd4134dea5b6dad3492c5c9e8b
noteisenheim <noteisenheim@gmail.com>
2020-09-28 16:39:34 +0300

    add processes

--- lab8-ntp/lamport.py
+++ lab8-ntp/lamport.py
@@ -4,13 +4,13 @@ from datetime import datetime
 
 def local_time(counter):
     # print current state
-    return ' (LAMPORT_TIME={}, LOCAL_TIME={})'.format(counter, datetime.now())
+    return ' (VECTOR_TIME={}, LOCAL_TIME={})'.format(counter, datetime.now())
 
 
 def calc_recv_timestamp(recv_time_stamp, counter):
     # update on receive
     for i in range(len(counter)):
-        counter[i] = max(recv_time_stamp[i], counter[i]) + 1
+        counter[i] = max(recv_time_stamp[i], counter[i])
     return counter
 
 
@@ -35,8 +35,53 @@ def recv_message(pipe, pid, counter):
     message, timestamp = pipe.recv()
     print("received")
     counter = calc_recv_timestamp(timestamp, counter)
+    counter[pid] += 1
     print('message received at ' + str(pid) + local_time(counter))
     return counter
+
+
+def process_a(pipe_ab):
+    # define counter
+    counter_a = [0, 0, 0]
+    # events
+    counter_a = send_message(pipe_ab, 0, counter_a)
+    counter_a = send_message(pipe_ab, 0, counter_a)
+    counter_a = event(0, counter_a)
+    counter_a = recv_message(pipe_ab, 0, counter_a)
+    counter_a = event(0, counter_a)
+    counter_a = event(0, counter_a)
+    counter_a = recv_message(pipe_ab, 0, counter_a)
+    # final counter
+    print('counter for PROCESS A {}'.format(counter_a))
+
+
+def process_b(pipe_ba, pipe_bc):
+    # define counter
+    counter_b = [0, 0, 0]
+    # events
+    counter_b = recv_message(pipe_ba, 1, counter_b)
+    counter_b = recv_message(pipe_ba, 1, counter_b)
+    counter_b = send_message(pipe_ba, 1, counter_b)
+    counter_b = recv_message(pipe_bc, 1, counter_b)
+    counter_b = event(1, counter_b)
+    counter_b = send_message(pipe_ba, 1, counter_b)
+    counter_b = send_message(pipe_bc, 1, counter_b)
+    counter_b = send_message(pipe_bc, 1, counter_b)
+    # final counter
+    print('counter for PROCESS B {}'.format(counter_b))
+
+
+def process_c(pipe_cb):
+    # define counter
+    counter_c = [0, 0, 0]
+    # events
+    counter_c = send_message(pipe_cb, 2, counter_c)
+    counter_c = recv_message(pipe_cb, 2, counter_c)
+    counter_c = event(2, counter_c)
+    counter_c = recv_message(pipe_cb, 2, counter_c)
+    # final counter
+    print('counter for PROCESS C {}'.format(counter_c))
+    
 
 
 if __name__ == '__main__':
@@ -48,50 +93,17 @@ if __name__ == '__main__':
     # pid b = 1
     # pid c = 2
 
-    # define counters
-    counter_a = [0, 0, 0]
-    counter_b = [0, 0, 0]
-    counter_c = [0, 0, 0]
+    # create processes
+    a_process = Process(target=process_a, args=(pipe_ab,))
+    b_process = Process(target=process_b, args=(pipe_ba, pipe_bc))
+    c_process = Process(target=process_c, args=(pipe_cb,))
 
-    # 0
-    counter_a = send_message(pipe_ab, 0, counter_a)
-    counter_c = send_message(pipe_bc, 2, counter_c)
+    # start processes
+    a_process.start()
+    b_process.start()
+    c_process.start()
 
-    # 1
-    counter_b = recv_message(pipe_ba, 1, counter_b)
-    counter_a = send_message(pipe_ab, 0, counter_a)
-
-    # 2
-    counter_b = recv_message(pipe_ba, 1, counter_b)
-    counter_a = event(0, counter_a)
-
-    # 3
-    counter_b = send_message(pipe_ab, 1, counter_b)
-
-    # 4
-    counter_a = recv_message(pipe_ba, 0, counter_a)
-    counter_b = recv_message(pipe_cb, 1, counter_b)
-
-    # 5
-    counter_a = event(0, counter_a)
-    counter_b = event(1, counter_b)
-
-    # 6
-    counter_a = event(0, counter_a)
-    counter_b = send_message(pipe_ab, 1, counter_b)
-
-    # 7
-    counter_a = recv_message(pipe_ba, 0, counter_a)
-    counter_b = send_message(pipe_bc, 1, counter_b)
-
-    # 8
-    counter_c = recv_message(pipe_cb, 2, counter_c)
-    counter_b = send_message(pipe_bc, 1, counter_b)
-
-    # 9 and 10
-    counter_c = event(2, counter_c)
-    counter_c = recv_message(pipe_cb, 2, counter_c)
-
-    print('counter for PROCESS A {}'.format(counter_a))
-    print('counter for PROCESS B {}'.format(counter_b))
-    print('counter for PROCESS C {}'.format(counter_c))
+    # join and wait for completion
+    a_process.join()
+    b_process.join()
+    c_process.join()
